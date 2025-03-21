76ed1414742eedd1731176c8737ac8f79b752307
Bruno Perković <perkanperkovic15@gmail.com>
2025-03-21 14:53:40 +0100

    Cleanup

--- Assets/Scripts/ComputeFluidSim.cs
+++ Assets/Scripts/ComputeFluidSim.cs
@@ -9,10 +9,21 @@ using UnityEditor;
 public class ComputePreviewer : MonoBehaviour
 {
     public static readonly int X = 1024;
-    
-    public ComputeShader computeShader;
-    public DrawKernel drawKernel;
 
+    public enum PlaybackMode
+    {
+        Step,
+        Fixed
+    };
+
+    [Header("Simulation settings")]
+
+    public PlaybackMode playbackMode;
+
+    public ComputeShader computeShader;
+    
+    [Header("Properties")]
+     
     public Vector2Int resolution;
     public int numParticles = 100;
     public float particleRadius = 0.01f;
@@ -24,24 +35,15 @@ public class ComputePreviewer : MonoBehaviour
     public float kernelRadius = 0.1f;
     public float restitutionCoeff = 0.99f;
 
-    public float scale = 1.0f; 
     public float pointerRadius = 0.3f;
     public float pointerStrength = 1.0f;
-    public Vector2 offset = Vector2.zero;
 
     private RenderTexture renderTexture;
-    private ComputeBuffer positionBuffer, predictedPositionBuffer, velocityBuffer, forceBuffer, densityBuffer, statsBuffer;
-
-    public enum DrawKernel
-    {
-        Particle = 0,
-        Density = 1,
-        Pressure = 2,
-    }
+    private ComputeBuffer positionBuffer, velocityBuffer, forceBuffer, densityBuffer, statsBuffer;
 
     public enum ComputeKernel
     {
-        RandomInit = 3,
+        Draw = 0,
         Step,
         PreStep,
     }
@@ -52,32 +54,25 @@ public class ComputePreviewer : MonoBehaviour
     {
         InitializeRenderTexture();
         InitializeBuffers();
+        InitializePositions();
         SetUniformData();
-        DispatchRandomInit();
+        //DispatchRandomInit();
         DispatchDraw();
     }
 
-    private void InitializeBuffers()
+    private void InitializePositions()
     {
-        positionBuffer = new ComputeBuffer(numParticles, sizeof(float) * 2);
-        predictedPositionBuffer = new ComputeBuffer(numParticles, sizeof(float) * 2);
-        velocityBuffer = new ComputeBuffer(numParticles, sizeof(float) * 2);
-        forceBuffer = new ComputeBuffer(numParticles, sizeof(float) * 2);
-        densityBuffer = new ComputeBuffer(numParticles, sizeof(float));
-        statsBuffer = new ComputeBuffer(stats.Length, sizeof(uint), ComputeBufferType.Raw);
-        statsBuffer.SetData(stats);
-
-        for (int k = 0; k < Enum.GetValues(typeof(ComputeKernel)).Length + Enum.GetValues(typeof(DrawKernel)).Length; k++)
+        Vector2[] initialPositions = new Vector2[numParticles];
+        int ceil = Mathf.CeilToInt(Mathf.Sqrt((float)numParticles));
+        float spacing = 0.5f / ceil;
+        for (int i = 0; i < numParticles; i++)
         {
-            computeShader.SetBuffer(k, "positions", positionBuffer);
-            computeShader.SetBuffer(k, "predictedPositions", predictedPositionBuffer);
-            computeShader.SetBuffer(k, "velocities", velocityBuffer);
-            computeShader.SetBuffer(k, "forces", forceBuffer);
-            computeShader.SetBuffer(k, "densities", densityBuffer);
-            computeShader.SetBuffer(k, "stats", statsBuffer);
-
-            computeShader.SetTexture(k, "result", renderTexture);
+            int x = i / ceil;
+            int y = i % ceil;
+            initialPositions[i] = new Vector2(spacing * x - 0.25f, spacing * y - 0.25f);
         }
+
+        positionBuffer.SetData(initialPositions);
     }
 
     void InitializeRenderTexture()
@@ -88,9 +83,29 @@ public class ComputePreviewer : MonoBehaviour
         GetComponentInChildren<Renderer>().material.mainTexture = renderTexture;
     }
 
+    private void InitializeBuffers()
+    {
+        positionBuffer = new ComputeBuffer(numParticles, sizeof(float) * 2);
+        velocityBuffer = new ComputeBuffer(numParticles, sizeof(float) * 2);
+        forceBuffer = new ComputeBuffer(numParticles, sizeof(float) * 2);
+        densityBuffer = new ComputeBuffer(numParticles, sizeof(float));
+        statsBuffer = new ComputeBuffer(stats.Length, sizeof(uint), ComputeBufferType.Raw);
+        statsBuffer.SetData(stats);
+
+        for (int k = 0; k < Enum.GetValues(typeof(ComputeKernel)).Length; k++)
+        {
+            computeShader.SetBuffer(k, "positions", positionBuffer);
+            computeShader.SetBuffer(k, "velocities", velocityBuffer);
+            computeShader.SetBuffer(k, "forces", forceBuffer);
+            computeShader.SetBuffer(k, "densities", densityBuffer);
+            computeShader.SetBuffer(k, "stats", statsBuffer);
+
+            computeShader.SetTexture(k, "result", renderTexture);
+        }
+    }
+
     void SetUniformData()
     {
-        computeShader.SetFloat("scale", scale);
         computeShader.SetFloat("restitutionCoeff", restitutionCoeff);
         computeShader.SetInt("numParticles", numParticles);
         computeShader.SetFloat("particleRadius", particleRadius);
@@ -99,11 +114,7 @@ public class ComputePreviewer : MonoBehaviour
         computeShader.SetFloat("pressureCoeff", pressureCoeff);
         computeShader.SetFloat("viscosityCoeff", viscosityCoeff);
         computeShader.SetFloat("gravityCoeff", gravityCoeff);
-        computeShader.SetVector("offset", offset);
         computeShader.SetFloat("deltaTime", Time.deltaTime);
-        computeShader.SetBool("pointerActive", Input.GetMouseButton(0));
-        computeShader.SetFloat("pointerRadius", pointerRadius);
-        computeShader.SetFloat("pointerStrength", pointerStrength);
 
         float kr2 = kernelRadius * kernelRadius;
         float kr3 = kr2 * kernelRadius;
@@ -114,44 +125,45 @@ public class ComputePreviewer : MonoBehaviour
         computeShader.SetFloat("kernelRadius3", kr3);
         computeShader.SetFloat("kernelRadius4", kr4);
         computeShader.SetFloat("kernelRadius5", kr5);
-    }
 
-    private void OnValidate()
-    {
-        updateCorners();
+        if (Input.GetMouseButton(0))
+        {
+            computeShader.SetBool("pointerActive", true);
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 3.0f))
+            {
+                computeShader.SetVector("pointerPos", inverseTransform(hit.point));
+            }
+            computeShader.SetFloat("pointerRadius", pointerRadius);
+            computeShader.SetFloat("pointerStrength", pointerStrength);
+        }
+        else
+        {
+            computeShader.SetBool("pointerActive", false);
+        }
     }
 
     private void Update()
     {
-        SetUniformData();
-        DispatchStep();
-        DispatchDraw();
-        DisplayStats();
+        if (Time.frameCount < 100)
+        {
+            return;
+        }
+
+        if (playbackMode == PlaybackMode.Fixed || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SetUniformData();
+            DispatchStep();
+            DispatchDraw();
+            DisplayStats();
+        }
     }
 
     private void OnDrawGizmos()
     {
-        if (!Application.isPlaying) return;
-
-        Vector2[] positions = new Vector2[10];
-        Vector2[] forces = new Vector2[10];
-
-        positionBuffer.GetData(positions);
-        forceBuffer.GetData(forces);
-
-        for (int i = 0; i < forces.Length; i++)
+        if (Application.isPlaying)
         {
-            var worldPos = transform.TransformPoint(positions[i]) + Vector3.back * 0.05f;
-            var worldDir = transform.TransformDirection(forces[i]);
-            Gizmos.DrawSphere(worldPos, 0.01f);
-            Gizmos.DrawLine(worldPos, worldPos + worldDir * 0.1f);
+            return;
         }
-
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 3.0f))
-        {
-            computeShader.SetVector("pointerPos", inverseTransform(hit.point));
-        }
-
     }
 
     private void DisplayStats()
@@ -168,38 +180,6 @@ public class ComputePreviewer : MonoBehaviour
         statsBuffer.SetData(stats);
     }
 
-    private void updateCorners()
-    {
-        // Get all TextMeshPro components in children
-        TMP_Text[] textMeshes = GetComponentsInChildren<TMP_Text>();
-
-        if (textMeshes.Length < 4)
-        {
-            Debug.LogError($"Not enough TextMeshPro children. Expected 4, got {textMeshes.Length}");
-            return;
-        }
-
-        // Assign each text object to a corner
-        for (int i = 0; i < 4; i++)
-        {
-            // Put to gameobject corners
-            Vector2 corner = textMeshes[i].transform.localPosition;
-            corner.x /= Mathf.Abs(corner.x);
-            corner.y /= Mathf.Abs(corner.y);
-            corner /= 2.0f;
-            corner += Vector2.one * 0.5f;
-            
-            // Transform
-            var pos = transformPosition(corner);
-            textMeshes[i].text = $"({pos.x:F2} | {pos.y:F2})";
-        }
-    }
-
-    private Vector2 transformPosition(Vector2 normalizedPos)
-    {
-        return (normalizedPos + offset) * scale;
-    }
-
     public Vector2 inverseTransform(Vector3 worldPos)
     {
         return transform.InverseTransformPoint(worldPos);
@@ -208,7 +188,6 @@ public class ComputePreviewer : MonoBehaviour
     private void OnDestroy()
     {
         positionBuffer.Release();
-        predictedPositionBuffer.Release();
         velocityBuffer.Release();
         forceBuffer.Release();
         densityBuffer.Release();
@@ -219,15 +198,7 @@ public class ComputePreviewer : MonoBehaviour
     {
         if (computeShader == null || renderTexture == null) return;
 
-        computeShader.Dispatch((int)drawKernel, Mathf.CeilToInt((float)(resolution.x * resolution.y) / X), 1, 1);
-        computeShader.Dispatch((int)DrawKernel.Particle, Mathf.CeilToInt((float)(resolution.x * resolution.y) / X), 1, 1);
-    }
-
-    void DispatchRandomInit()
-    {
-        if (computeShader == null || renderTexture == null) return;
-
-        computeShader.Dispatch((int)ComputeKernel.RandomInit, Mathf.CeilToInt((float)(numParticles) / X), 1, 1);
+        computeShader.Dispatch((int)ComputeKernel.Draw, Mathf.CeilToInt((float)(resolution.x * resolution.y) / X), 1, 1);
     }
 
     void DispatchStep()
